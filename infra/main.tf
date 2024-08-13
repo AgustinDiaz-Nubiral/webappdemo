db9b71c3bd2e1d67f0ae1ca270c854bab8129d6c
AgustinDiaz-Nubiral <agustin.diaz@nubiral.com>
2024-08-12 22:17:47 -0300

    update site_config web app

--- infra/main.tf
+++ infra/main.tf
@@ -27,7 +27,7 @@ resource "azurerm_resource_group" "rg" {
 #Create Azure Container Registry-------------------------------------------------------------------------------
 
 resource "azurerm_container_registry" "acr" {
-  name                = "acrappdemopoc"
+  name                = "acrappdemo-adiaz"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
@@ -40,7 +40,7 @@ resource "azurerm_container_registry" "acr" {
 #Create Service Plan------------------------------------------------------------------------------------------
 
 resource "azurerm_service_plan" "appplan" {
-  name                = "appdemo-appserviceplan"
+  name                = "appdemo-appserviceplan-adiaz"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
@@ -53,7 +53,7 @@ resource "azurerm_service_plan" "appplan" {
 #Create Web App-----------------------------------------------------------------------------------------------
 
 resource "azurerm_linux_web_app" "webappoc" {
-  name                = "webapppocdemo"
+  name                = "webappdemo-adiaz"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.appplan.id
@@ -63,18 +63,25 @@ resource "azurerm_linux_web_app" "webappoc" {
   site_config {
     minimum_tls_version = "1.2"
     always_on = true
+    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/my-app:latest"
   }
-  identity {
-    type = "SystemAssigned"
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL"      = "https://${azurerm_container_registry.acr.login_server}"
+    "DOCKER_REGISTRY_SERVER_USERNAME" = azurerm_container_registry.acr.admin_username
+    "DOCKER_REGISTRY_SERVER_PASSWORD" = azurerm_container_registry.acr.admin_password
   }
 
-  
+  tags = {
+    environment = "production"
+  }
 }
+
+
 # Asignar permisos ACR Pull para la Web App ------------------------------------------------------------------
 
-resource "azurerm_role_assignment" "acr_pull" {
+/*resource "azurerm_role_assignment" "acr_pull" {
   principal_id         = azurerm_linux_web_app.webappoc.identity[0].principal_id
   role_definition_name = "AcrPull"
   scope                = azurerm_container_registry.acr.id
-}
+}*/
   
