587579e9d45add244c554f858d88e1edd1a58bf6
AgustinDiaz-Nubiral <agustin.diaz@nubiral.com>
2024-08-16 16:50:28 -0300

    update deployinfra

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,11 +1,11 @@
-terraform {
+/*terraform {
   required_providers {
-    azurerm = {
+     azurerm = {
       source  = "hashicorp/azurerm"
       version = "=3.0.0"
     }
   }
-}
+}*/
 
 provider "azurerm" {
   features {}
